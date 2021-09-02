a90dac85648aff19ac9bfbf476599d4a69420d34
Apoorv Srivastava <apoorvsrivastava2001@gmail.com>
2021-09-02 10:16:27 +0530

    rec complete

---
+++ josephus.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    static int myjos(int n,int k){
+        return jos(n,k)+1;
+    }
+    static int jos(int n, int k){
+        if(n==1){
+            return 0;
+        }
+        else{
+            return (jos(n-1,k)+k)%n;
+        }
+        
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		System.out.print(jos(n,k));
+	}
+}
