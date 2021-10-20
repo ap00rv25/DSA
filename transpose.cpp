0237b0b2a76962af523e33023c3596f8819d80eb
Apoorv Srivastava <apoorvsrivastava2001@gmail.com>
2021-10-20 16:37:40 +0530

    transpose

---
+++ transpose.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int n=4;
+void transpose(int arr[n][n])
+{
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            swap(arr[i][j],arr[j][i]);
+        }
+        
+    }
+    
+}
+
+int main() {
+    int arr[n][n] = {{1, 2, 3, 4},
+				   {5, 6, 7, 8},
+				   {9, 10, 11, 12},
+				   {13, 14, 15, 16}};
+
+    transpose(arr);
+    for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+
+		cout << endl;
+	}
+	return 0;
+}
