36c69c2ad48db4b76eaf8af185d020ce3316b127
ap00rv25 <62295920+ap00rv25@users.noreply.github.com>
2022-01-06 19:04:15 +0530

    Binary Search Tree

---
+++ serializebt.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>         // convert a binary tree into a vector //
+using namespace std;
+
+struct Node {
+    int key;
+    Node *left;
+    Node *right;
+    Node (int k) {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+const int empty = -1;
+void serialize(Node *root, vector<int> &arr) {
+    if(root==NULL) {
+        arr.push_back(empty);
+        return;
+    }
+    arr.push_back(root->key);
+    serialize(root->left,arr);
+    serialize(root->right,arr);
+}
+
+int main() {
+    Node *root = new Node(10);
+    root -> left = new Node(20);
+    root -> right = new Node(30);
+    root -> right -> left = new Node(40);
+    root -> right -> right = new Node(50);
+    root -> left -> left = new Node(60);
+    root -> left -> right = new Node(70);
+    root -> left -> right -> right = new Node(80);
+    vector<int> arr;
+	serialize(root,arr);
+	for (int x : arr) {
+	    cout<<x<<" ";
+	}
+	return 0;
+} 
