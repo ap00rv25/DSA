294975c4575b87634025556eb01da310180d1f20
ap00rv25 <62295920+ap00rv25@users.noreply.github.com>
2022-01-13 11:26:45 +0530

    printkth smallest in bst

---
+++ printkth.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int key;
+    Node *left;
+    Node *right;
+    Node(int k) {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+void printkth(Node *root, int k, int &count) {
+    if(root!=NULL) {
+        printkth(root->left,k,count);
+        count++;
+        if(count==k) {
+            cout<<root->key;
+            return;
+        }
+        printkth(root->right,k,count);
+    }
+}
+
+int main() {
+    Node *root=new Node(15);
+	root->left=new Node(5);
+	root->left->left=new Node(3);
+	root->right=new Node(20);
+	root->right->left=new Node(18);
+	root->right->left->left=new Node(16);
+	root->right->right=new Node(80);
+	int k=3;
+	int count=0;
+	cout<<"Kth Smallest: ";
+	printkth(root,k,count);
+    return 0;
+}
