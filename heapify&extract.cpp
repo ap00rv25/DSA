3ef02e7d0f2eeec34bad5a32bfd2fdd3378dbad6
ap00rv25 <62295920+ap00rv25@users.noreply.github.com>
2022-01-23 19:03:11 +0530

    heapify and extract minimum element

---
+++ heapify&extract.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MinHeap{
+    int *arr;
+    int size,capacity;
+    public:
+    MinHeap (int c){
+        size = 0;
+        capacity = c;
+        arr = new int[c];
+    }
+    int left(int i) {return (2*i+1);}
+    int right(int i) {return (2*i+2);}
+    int parent(int i) {return (i-1/2);}
+    void insert (int x){
+        if(size==capacity) return;
+        size++;
+        arr[size-1]=x;
+        for(int i=size-1; i!=0 && arr[parent(i)]>arr[i];) {
+            swap(arr[i],arr[parent(i)]);
+            i = parent(i);
+        }
+    }
+    void minHeapify(int i) {
+        int lt=left(i),rt=right(i);
+        int smallest = i;
+        if(lt < size && arr[lt]<arr[i]) smallest = lt;
+        if(rt < size && arr[rt]<arr[smallest]) smallest = rt;
+        if(smallest!=i){
+            swap(arr[i],arr[smallest]);
+            minHeapify(smallest);
+        }
+    }
+    int extractmin() {
+        if(size==0) return INT_MAX;
+        if(size==1) {
+            size--;
+            return arr[0];
+        }
+        swap(arr[0],arr[size-1]);
+        size--;
+        minHeapify(0);
+        return arr[size];
+    }
+};
+int main() 
+{ 
+    MinHeap h(11);
+    h.insert(3); 
+    h.insert(2);
+    h.insert(15);
+    h.insert(20);
+    cout << h.extractmin() << " ";
+    return 0;
+}
