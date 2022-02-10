e24c7b51d1e49c24e94fe6a3a92ef5f5d4e2a46b
ap00rv25 <62295920+ap00rv25@users.noreply.github.com>
2022-02-10 16:02:41 +0530

    anagram

---
+++ anagram.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+bool areAnagram(string &str1, string &str2) {
+    if(str1.length()!=str2.length()) return false;
+    sort(str1.begin(),str1.end());
+    sort(str2.begin(),str2.end());
+    return (str1==str2);
+} 
+ 
+int main() 
+{ 
+    string str1 = "abaac"; 
+    string str2 = "aacba"; 
+    if (areAnagram(str1, str2)) 
+        cout << "The two strings are anagram of each other"; 
+    else
+        cout << "The two strings are not anagram of each other"; 
+  
+    return 0; 
+}
