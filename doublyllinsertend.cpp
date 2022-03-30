9a2a5d638ed486efe8465ececb3fd89a3db3a6c1
ap00rv25 <62295920+ap00rv25@users.noreply.github.com>
2022-03-30 15:45:41 +0530

    Doubly Linked List Insertion at End

---
+++ doublyllinsertend.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int d) {
+        data = d;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+Node *insertend(Node *head , int x) {
+    Node *temp = new Node(x);
+    if(head==NULL) return temp;
+    Node *curr = head;
+    while(curr->next!=NULL) curr=curr->next;
+    curr->next=temp;
+    temp->prev=curr;
+    return head;
+}
+
+void printlist(Node *head) {
+    Node *curr = head;
+    while(curr!=NULL) {
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+    cout<<endl;
+}
+
+int main() {
+    Node *head=new Node(10);
+	Node *temp1=new Node(20);
+	Node *temp2=new Node(30);
+	head->next=temp1;
+	temp1->prev=head;
+	temp1->next=temp2;
+	temp2->prev=temp1;
+	head=insertend(head,40);
+	printlist(head);
+	// your code goes here
+	return 0;
+}
